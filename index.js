a7e814f957b54f6ca39e76cc19cc3d8fe464163a
codingspirits <vincci.cheng.9@gmail.com>
2021-07-17 15:58:33 +1000

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(names, occasion) {
+    let message = []
+    for (let i=0; i<names.length; i++){
+        message.push(`Thankyou, ${names[i]}, for the wonderful ${occasion} gift!`)
+    }
+    console.log(message)
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday")
+
+function countDown(num){
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
+
+countDown(10)
